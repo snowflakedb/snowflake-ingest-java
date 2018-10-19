452f10ae86d0527bcb774af99450eb18e7de5f08
Bing Li <binglihub@gmail.com>
2018-10-19 15:23:47 -0700

    fixed command not found

--- scripts/run_travis.sh
+++ scripts/run_travis.sh
@@ -1,8 +1,5 @@
-travis_fold_start build "Build Ingest SDK Java driver"
 mvn install -DskipTests=true --batch-mode --show-version
-travis_fold_end
 
-travis_fold_start build "Test Ingest SDK Java driver"
 PARAMS=()
 PARAMS+=("-DtravisIT")
 echo "JDK Version: $TRAVIS_JDK_VERSION"
@@ -10,4 +7,4 @@ echo "JDK Version: $TRAVIS_JDK_VERSION"
 # verify phase is after test/integration-test phase, which means both unit test 
 # and integration test will be run
 mvn "${PARAMS[@]}" verify --batch-mode
-travis_fold_end
+
