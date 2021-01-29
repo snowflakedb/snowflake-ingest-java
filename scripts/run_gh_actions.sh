839f82c3da43026516b36df5fdbfe8d10599a54d
Jay Patel <jay.patel@snowflake.com>
2021-01-29 15:08:52 -0800

    Migrate CI to Github actions (#75)
    Migrate travis CI to Github actions
    
    Test:
    
    I have tested this locally on act. https://github.com/nektos/act
    The profile.json gpg is working as I tried testing with unencrypted version.

---
+++ scripts/run_gh_actions.sh
@@ -0,0 +1,16 @@
+mvn install -DskipTests=true --batch-mode --show-version
+
+PARAMS=()
+PARAMS+=("-DtravisIT")
+# testing will not need shade dep. otherwise codecov cannot work
+PARAMS+=("-Dnot-shadeDep")
+echo "JDK Version: $TRAVIS_JDK_VERSION"
+[[ -n "$JACOCO_COVERAGE" ]] && PARAMS+=("-Djacoco.skip.instrument=false")
+# verify phase is after test/integration-test phase, which means both unit test
+# and integration test will be run
+mvn "${PARAMS[@]}" verify --batch-mode
+
+# run whitesource
+echo ${PWD}
+chmod 755 ./scripts/run_whitesource_gh.sh
+./scripts/run_whitesource_gh.sh
