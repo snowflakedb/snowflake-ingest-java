aa547695bbcc8b98eef2bae6b4d1e4d260655c8c
Jay Patel <jay.patel@snowflake.com>
2021-02-01 17:15:15 -0800

    Fail github actions on mvn verify failure

--- scripts/run_gh_actions.sh
+++ scripts/run_gh_actions.sh
@@ -1,14 +1,18 @@
 mvn install -DskipTests=true --batch-mode --show-version
 
 PARAMS=()
-PARAMS+=("-DtravisIT")
+PARAMS+=("-DghActionsIT")
 # testing will not need shade dep. otherwise codecov cannot work
 PARAMS+=("-Dnot-shadeDep")
-echo "JDK Version: $TRAVIS_JDK_VERSION"
 [[ -n "$JACOCO_COVERAGE" ]] && PARAMS+=("-Djacoco.skip.instrument=false")
 # verify phase is after test/integration-test phase, which means both unit test
 # and integration test will be run
 mvn "${PARAMS[@]}" verify --batch-mode
+
+rc=$?
+if [ $rc -ne 0 ] ; then
+  echo Could not perform mvn verify with parameters "${PARAMS[@]}", exit code [$rc]; exit $rc
+fi
 
 # run whitesource
 echo ${PWD}
