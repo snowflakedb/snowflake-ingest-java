440aceb4e863f778df1e46ed2b2cf0bfa9d59f54
sfc-gh-mnaides <matthew.naides@snowflake.com>
2021-05-05 22:15:02 +0000

    fix copyright year

--- src/main/java/net/snowflake/ingest/streaming/internal/SnowflakeURL.java
+++ src/main/java/net/snowflake/ingest/streaming/internal/SnowflakeURL.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 Snowflake Computing Inc. All rights reserved.
+ * Copyright (c) 2021 Snowflake Computing Inc. All rights reserved.
  */
 
 package net.snowflake.ingest.streaming.internal;
