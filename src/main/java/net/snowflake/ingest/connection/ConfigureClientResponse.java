aa26f5e24cd4686efa3eb81ebf5f08b0ee524895
Andy Wang <andy.wang@snowflake.com>
2021-11-16 17:40:28 -0500

    remove todo, no need to add requestId

--- src/main/java/net/snowflake/ingest/connection/ConfigureClientResponse.java
+++ src/main/java/net/snowflake/ingest/connection/ConfigureClientResponse.java
@@ -7,8 +7,6 @@ import java.util.UUID;
 /** ConfigureClientResponse - response from a configure client request */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ConfigureClientResponse {
-  // Todo - add requestId to client endpoints response
-
   private Long clientSequencer;
 
   @Override
