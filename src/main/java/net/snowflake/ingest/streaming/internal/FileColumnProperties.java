2c29cfa2447fed0661c3cd461001c45d2889073a
Toby Zhang <toby.zhang@snowflake.com>
2022-03-25 23:42:24 +0000

    fix null columns issue

--- src/main/java/net/snowflake/ingest/streaming/internal/FileColumnProperties.java
+++ src/main/java/net/snowflake/ingest/streaming/internal/FileColumnProperties.java
@@ -35,10 +35,19 @@ class FileColumnProperties {
   // for binary or string columns
   private long maxLength;
 
+  // Default value to use for min/max int when all data in the given column is NULL
+  private static final BigInteger defaultMinMaxIntVal = BigInteger.valueOf(0);
+
   FileColumnProperties(RowBufferStats stats) {
     this.setCollation(stats.getCollationDefinitionString());
-    this.setMaxIntValue(stats.getCurrentMaxIntValue());
-    this.setMinIntValue(stats.getCurrentMinIntValue());
+    this.setMaxIntValue(
+        stats.getCurrentMaxIntValue() == null
+            ? defaultMinMaxIntVal
+            : stats.getCurrentMaxIntValue());
+    this.setMinIntValue(
+        stats.getCurrentMinIntValue() == null
+            ? defaultMinMaxIntVal
+            : stats.getCurrentMinIntValue());
     this.setMinRealValue(stats.getCurrentMinRealValue());
     this.setMaxRealValue(stats.getCurrentMaxRealValue());
     this.setMaxLength(stats.getCurrentMaxLength());
