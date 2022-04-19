37bd7bcb6c095a8b07d9bfdaacdf24369f5458f0
Matt Naides <63469689+sfc-gh-mnaides@users.noreply.github.com>
2022-04-19 14:58:42 -0700

    SNOW-556750 retry certain streaming ingest specific retries (#158)
    * add ingest specific code retry
    
    * add unit tests
    
    * checkpoint
    
    * checkpoint
    
    * update test
    
    * use Pair instead of bespoke pojo
    
    * pr feedback
    
    * test update

---
+++ src/main/java/net/snowflake/ingest/streaming/internal/StreamingIngestResponse.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2022 Snowflake Computing Inc. All rights reserved.
+ */
+
+package net.snowflake.ingest.streaming.internal;
+
+abstract class StreamingIngestResponse {
+  abstract Long getStatusCode();
+}
