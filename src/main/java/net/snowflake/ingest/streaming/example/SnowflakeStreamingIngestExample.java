7bfb1654e881e87f1a9754c9744669d30fcf7c42
Jay Patel <jay.patel@snowflake.com>
2022-05-09 11:31:23 -0700

    Review comments

--- src/main/java/net/snowflake/ingest/streaming/example/SnowflakeStreamingIngestExample.java
+++ src/main/java/net/snowflake/ingest/streaming/example/SnowflakeStreamingIngestExample.java
@@ -21,10 +21,10 @@ import net.snowflake.ingest.streaming.SnowflakeStreamingIngestClientFactory;
 /**
  * Example on how to use the Streaming Ingest client APIs.
  *
- * <p>Please read README.md file for detailed steps
+ * <p>Please read the README.md file for detailed steps
  */
 public class SnowflakeStreamingIngestExample {
-  private static String PROFILE_PATH = "profile.json";
+  private static String PROFILE_PATH = "profile_streaming.json";
   private static final ObjectMapper mapper = new ObjectMapper();
 
   public static void main(String[] args) throws Exception {
@@ -39,54 +39,46 @@ public class SnowflakeStreamingIngestExample {
     // Create a streaming ingest client
     try (SnowflakeStreamingIngestClient client =
         SnowflakeStreamingIngestClientFactory.builder("CLIENT").setProperties(props).build()) {
+
       // Create an open channel request on table T_STREAMINGINGEST
       OpenChannelRequest request1 =
-          OpenChannelRequest.builder("CHANNEL")
-              .setDBName("DB_STREAMINGINGEST")
-              .setSchemaName("SCHEMA_STREAMINGINGEST")
-              .setTableName("T_STREAMINGINGEST")
+          OpenChannelRequest.builder("MY_CHANNEL")
+              .setDBName("MY_DATABASE")
+              .setSchemaName("MY_SCHEMA")
+              .setTableName("MY_TABLE")
               .setOnErrorOption(OpenChannelRequest.OnErrorOption.CONTINUE)
               .build();
 
       // Open a streaming ingest channel from the given client
       SnowflakeStreamingIngestChannel channel1 = client.openChannel(request1);
 
+      // Insert few rows into the channel (Using insertRows API), along with the offset Token which
+      // corresponds to the row number
       final int totalRowsInTable = 1000;
-
-      // Insert a few rows into the channel,
       for (int val = 0; val < totalRowsInTable; val++) {
         Map<String, Object> row = new HashMap<>();
 
         // c1 corresponds to the column name in table
         row.put("c1", val);
 
-        // Along with row, we are also passing in offset Token which corresponds to the row number
-        // for simplicity
         InsertValidationResponse response = channel1.insertRow(row, String.valueOf(val));
         if (response.hasErrors()) {
           // Simply throw if there is an exception
           throw response.getInsertErrors().get(0).getException();
         }
       }
+
+      // Polling Snowflake to fetch offset token registered in Snowflake
       final int expectedOffsetTokenInSnowflake = 999; // because it goes from 0 to 999
       String offsetTokenFromSnowflake = channel1.getLatestCommittedOffsetToken();
 
-      // Note: This is just an upper bound used as an example. Please don't use this for
-      // benchmarking.
       final int maxRetries = 10;
       int retryCount = 0;
       while (offsetTokenFromSnowflake == null
           || !offsetTokenFromSnowflake.equals(String.valueOf(expectedOffsetTokenInSnowflake))) {
-
-        // Sleeping for 1 second to Commit all rows in the table.
         Thread.sleep(1_000);
-        // Used as a polling
         offsetTokenFromSnowflake = channel1.getLatestCommittedOffsetToken();
         retryCount++;
-        System.out.println(
-            String.format(
-                "OffsetToken found in Snowflake:%s with retryCount:%s",
-                offsetTokenFromSnowflake, retryCount));
         if (retryCount >= maxRetries) {
           System.out.println(
               String.format(
