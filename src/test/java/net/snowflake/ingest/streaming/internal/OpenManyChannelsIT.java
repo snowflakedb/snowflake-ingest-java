d58353b30aabfbf94e8b84806e3798635174ae02
Lukas Sembera <lukas.sembera@snowflake.com>
2022-11-28 17:31:17 +0000

    Test that data can only be ingested into the highest client sequencer

--- src/test/java/net/snowflake/ingest/streaming/internal/OpenManyChannelsIT.java
+++ src/test/java/net/snowflake/ingest/streaming/internal/OpenManyChannelsIT.java
@@ -5,16 +5,20 @@ import static net.snowflake.ingest.utils.Constants.ROLE;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 import net.snowflake.ingest.TestUtils;
 import net.snowflake.ingest.streaming.OpenChannelRequest;
 import net.snowflake.ingest.streaming.SnowflakeStreamingIngestChannel;
 import net.snowflake.ingest.streaming.SnowflakeStreamingIngestClient;
 import net.snowflake.ingest.streaming.SnowflakeStreamingIngestClientFactory;
 import net.snowflake.ingest.utils.Constants;
+import net.snowflake.ingest.utils.SFException;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.Assert;
@@ -54,14 +58,17 @@ public class OpenManyChannelsIT {
 
   /**
    * Reopens the same channel from multiple threads, asserts each channel has a unique client
-   * sequencer
+   * sequencer. At the end it verifies that only the channel with highest client sequencer can
+   * ingest data
    */
   @Test
   public void reopenSameChannel() throws Exception {
     String channelName = "CHANNEL";
     List<Thread> threads = new ArrayList<>();
     List<Exception> exceptions = Collections.synchronizedList(new ArrayList<>());
-    Set<Long> channelSequencers = Collections.synchronizedSet(new HashSet<>());
+    ConcurrentHashMap<Integer, SnowflakeStreamingIngestChannel> clientSequencerToChannelMap =
+        new ConcurrentHashMap<>();
+
     for (int i = 0; i < THREAD_COUNT; i++) {
       Thread t =
           new Thread(
@@ -80,7 +87,7 @@ public class OpenManyChannelsIT {
                     Long channelSequencer =
                         ((SnowflakeStreamingIngestChannelInternal<Void>) channel)
                             .getChannelSequencer();
-                    channelSequencers.add(channelSequencer);
+                    clientSequencerToChannelMap.put(channelSequencer.intValue(), channel);
                   } catch (Exception e) {
                     exceptions.add(e);
                     break;
@@ -102,8 +109,31 @@ public class OpenManyChannelsIT {
       Assert.fail(String.format("Exceptions thrown: %d", exceptions.size()));
     }
 
-    // Verify that each channel reopen received its own sequencer
-    Assert.assertEquals(THREAD_COUNT * CHANNELS_PER_THREAD, channelSequencers.size());
+    // Verify that each reopened channel received its own sequencer
+    Assert.assertEquals(THREAD_COUNT * CHANNELS_PER_THREAD, clientSequencerToChannelMap.size());
+
+    // Verify that ingestion into to the channel with the highest client sequencer works
+    int highestSequencer =
+        clientSequencerToChannelMap.keySet().stream().max(Comparator.naturalOrder()).get();
+    Assert.assertEquals(THREAD_COUNT * CHANNELS_PER_THREAD - 1, highestSequencer);
+    String offsetToken = UUID.randomUUID().toString();
+    SnowflakeStreamingIngestChannel lastOpenedChannel =
+        clientSequencerToChannelMap.get(highestSequencer);
+    lastOpenedChannel.insertRow(new HashMap<>(), offsetToken);
+    TestUtils.waitForOffset(lastOpenedChannel, offsetToken);
+
+    // Verify that ingestion into all other channels does not work
+    for (Map.Entry<Integer, SnowflakeStreamingIngestChannel> entry :
+        clientSequencerToChannelMap.entrySet()) {
+      if (entry.getKey() != highestSequencer) {
+        try {
+          entry.getValue().insertRow(new HashMap<>(), UUID.randomUUID().toString());
+          Assert.fail("Ingestion into channel with non-latest client sequencer should fail");
+        } catch (SFException e) {
+          // all good, expected exception has been thrown
+        }
+      }
+    }
   }
 
   /** Opens many channels in parallel, checks that each has client sequencer 0 */
