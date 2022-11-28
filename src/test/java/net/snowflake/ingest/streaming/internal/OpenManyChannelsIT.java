903a4c4f933fc83d5b151f2c49ededaea7ece5ed
Lukas Sembera <lukas.sembera@snowflake.com>
2022-11-28 14:17:43 +0000

    Test that reopened channels have different client sequencer

--- src/test/java/net/snowflake/ingest/streaming/internal/OpenManyChannelsIT.java
+++ src/test/java/net/snowflake/ingest/streaming/internal/OpenManyChannelsIT.java
@@ -1,14 +1,17 @@
-package net.snowflake.ingest.streaming.internal.datatypes;
+package net.snowflake.ingest.streaming.internal;
 
 import static net.snowflake.ingest.utils.Constants.ROLE;
 
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Properties;
+import java.util.Set;
 import net.snowflake.ingest.TestUtils;
 import net.snowflake.ingest.streaming.OpenChannelRequest;
+import net.snowflake.ingest.streaming.SnowflakeStreamingIngestChannel;
 import net.snowflake.ingest.streaming.SnowflakeStreamingIngestClient;
 import net.snowflake.ingest.streaming.SnowflakeStreamingIngestClientFactory;
 import net.snowflake.ingest.utils.Constants;
@@ -49,8 +52,63 @@ public class OpenManyChannelsIT {
     client = SnowflakeStreamingIngestClientFactory.builder("client1").setProperties(props).build();
   }
 
+  /**
+   * Reopens the same channel from multiple threads, asserts each channel has a unique client
+   * sequencer
+   */
   @Test
-  public void testOpenManyChannels() throws Exception {
+  public void reopenSameChannel() throws Exception {
+    String channelName = "CHANNEL";
+    List<Thread> threads = new ArrayList<>();
+    List<Exception> exceptions = Collections.synchronizedList(new ArrayList<>());
+    Set<Object> channelSequencers = Collections.synchronizedSet(new HashSet<>());
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      Thread t =
+          new Thread(
+              () -> {
+                for (int j = 0; j < CHANNELS_PER_THREAD; j++) {
+                  OpenChannelRequest openChannelRequest =
+                      OpenChannelRequest.builder(channelName)
+                          .setDBName(databaseName)
+                          .setSchemaName(SCHEMA_NAME)
+                          .setTableName(TABLE_NAME)
+                          .setOnErrorOption(OpenChannelRequest.OnErrorOption.ABORT)
+                          .build();
+                  try {
+                    SnowflakeStreamingIngestChannel channel =
+                        client.openChannel(openChannelRequest);
+                    Long channelSequencer =
+                        ((SnowflakeStreamingIngestChannelInternal<Void>) channel)
+                            .getChannelSequencer();
+                    channelSequencers.add(channelSequencer);
+                  } catch (Exception e) {
+                    exceptions.add(e);
+                    break;
+                  }
+                }
+              });
+      t.start();
+      threads.add(t);
+    }
+
+    for (Thread t : threads) {
+      t.join();
+    }
+
+    if (!exceptions.isEmpty()) {
+      for (Exception e : exceptions) {
+        e.printStackTrace();
+      }
+      Assert.fail(String.format("Exceptions thrown: %d", exceptions.size()));
+    }
+
+    // Verify that each channel reopen received its own sequencer
+    Assert.assertEquals(THREAD_COUNT * CHANNELS_PER_THREAD, channelSequencers.size());
+  }
+
+  /** Opens many channels in parallel, checks that each has client sequencer 0 */
+  @Test
+  public void testOpenManyDifferentChannels() throws Exception {
     List<Thread> threads = new ArrayList<>();
     List<Exception> exceptions = Collections.synchronizedList(new ArrayList<>());
     for (int i = 0; i < THREAD_COUNT; i++) {
@@ -63,11 +121,16 @@ public class OpenManyChannelsIT {
                       OpenChannelRequest.builder(String.format("CHANNEL-%s-%s", threadId, j))
                           .setDBName(databaseName)
                           .setSchemaName(SCHEMA_NAME)
-                          .setTableName(TABLE_NAME + "a")
+                          .setTableName(TABLE_NAME)
                           .setOnErrorOption(OpenChannelRequest.OnErrorOption.ABORT)
                           .build();
                   try {
-                    client.openChannel(openChannelRequest);
+                    SnowflakeStreamingIngestChannel channel =
+                        client.openChannel(openChannelRequest);
+                    Long channelSequencer =
+                        ((SnowflakeStreamingIngestChannelInternal<Void>) channel)
+                            .getChannelSequencer();
+                    Assert.assertEquals(0L, channelSequencer.longValue());
                   } catch (Exception e) {
                     exceptions.add(e);
                     break;
