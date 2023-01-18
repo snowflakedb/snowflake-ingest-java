5c64846b22c03ec7d402392b2e787e12ef7e0e71
Gloria Doci <gloria.doci@snowflake.com>
2023-01-18 14:57:13 +0000

    use other cofs

--- src/main/java/net/snowflake/ingest/streaming/example/ParquetPerf.java
+++ src/main/java/net/snowflake/ingest/streaming/example/ParquetPerf.java
@@ -9,42 +9,18 @@ public class ParquetPerf {
   public static Collection<Object[]> getParameters() {
     return Arrays.asList(
         new Object[][] {
-          // 100k X 100
-          {"Arrow", false, Constants.BdecVersion.ONE, 1000, 100, 10},
-          {"Parquet", false, Constants.BdecVersion.THREE, 1000, 100, 10},
-          {"Parquet", true, Constants.BdecVersion.THREE, 1000, 100, 10},
           // 1M X 1
           {"Arrow", false, Constants.BdecVersion.ONE, 10000, 100, 1},
           {"Parquet", false, Constants.BdecVersion.THREE, 10000, 100, 1},
           {"Parquet", true, Constants.BdecVersion.THREE, 10000, 100, 1},
-          // 10k X 100
-          {"Arrow", false, Constants.BdecVersion.ONE, 1000, 10, 100},
-          {"Parquet", false, Constants.BdecVersion.THREE, 1000, 10, 100},
-          {"Parquet", true, Constants.BdecVersion.THREE, 1000, 10, 100},
-          // 100k X 36
-          {"Arrow", false, Constants.BdecVersion.ONE, 1000, 100, 36},
-          {"Parquet", false, Constants.BdecVersion.THREE, 1000, 100, 36},
-          {"Parquet", true, Constants.BdecVersion.THREE, 1000, 100, 36},
-          // 100k X 1
-          {"Arrow", false, Constants.BdecVersion.ONE, 1000, 100, 1},
-          {"Parquet", false, Constants.BdecVersion.THREE, 1000, 100, 1},
-          {"Parquet", true, Constants.BdecVersion.THREE, 1000, 100, 1},
-          // 100k X 2
-          {"Arrow", false, Constants.BdecVersion.ONE, 1000, 100, 2},
-          {"Parquet", false, Constants.BdecVersion.THREE, 1000, 100, 2},
-          {"Parquet", true, Constants.BdecVersion.THREE, 1000, 100, 2},
-          // 100k X 3
-          {"Arrow", false, Constants.BdecVersion.ONE, 1000, 100, 3},
-          {"Parquet", false, Constants.BdecVersion.THREE, 1000, 100, 3},
-          {"Parquet", true, Constants.BdecVersion.THREE, 1000, 100, 3},
-          // 100k X 4
-          {"Arrow", false, Constants.BdecVersion.ONE, 1000, 100, 4},
-          {"Parquet", false, Constants.BdecVersion.THREE, 1000, 100, 4},
-          {"Parquet", true, Constants.BdecVersion.THREE, 1000, 100, 4},
-          // 100k X 5
-          {"Arrow", false, Constants.BdecVersion.ONE, 1000, 100, 5},
-          {"Parquet", false, Constants.BdecVersion.THREE, 1000, 100, 5},
-          {"Parquet", true, Constants.BdecVersion.THREE, 1000, 100, 5}
+          // 1M X 2
+          {"Arrow", false, Constants.BdecVersion.ONE, 10000, 100, 2},
+          {"Parquet", false, Constants.BdecVersion.THREE, 10000, 100, 2},
+          {"Parquet", true, Constants.BdecVersion.THREE, 10000, 100, 2},
+          // 1M X 2
+          {"Arrow", false, Constants.BdecVersion.ONE, 10000, 100, 3},
+          {"Parquet", false, Constants.BdecVersion.THREE, 10000, 100, 3},
+          {"Parquet", true, Constants.BdecVersion.THREE, 10000, 100, 3},
         });
   }
 
