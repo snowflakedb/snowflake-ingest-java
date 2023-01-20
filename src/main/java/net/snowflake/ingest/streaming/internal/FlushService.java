b187a4d9585bf0b0df286cbd16a13788cd5f4fec
Toby Zhang <toby.zhang@snowflake.com>
2023-01-20 07:39:28 +0000

    ee

--- src/main/java/net/snowflake/ingest/streaming/internal/FlushService.java
+++ src/main/java/net/snowflake/ingest/streaming/internal/FlushService.java
@@ -19,7 +19,6 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -34,7 +33,6 @@ import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
@@ -344,28 +342,25 @@ class FlushService<T> {
 
     while (itr.hasNext()) {
       List<List<ChannelData<T>>> blobData = new ArrayList<>();
-      AtomicReference<Float> totalBufferSize = new AtomicReference<>((float) 0);
+      float totalBufferSize = 0;
 
       final String filePath = getFilePath(this.targetStage.getClientPrefix());
 
       // Distribute work at table level, create a new blob if reaching the blob size limit
-      while (itr.hasNext() && totalBufferSize.get() <= MAX_BLOB_SIZE_IN_BYTES) {
+      while (itr.hasNext() && totalBufferSize <= MAX_BLOB_SIZE_IN_BYTES) {
         ConcurrentHashMap<String, SnowflakeStreamingIngestChannelInternal<T>> table =
             itr.next().getValue();
-        List<ChannelData<T>> channelsDataPerTable = Collections.synchronizedList(new ArrayList<>());
-        // Use parallel stream since getData could be the performance bottleneck when the number of
-        // channels are big
-        table.values().parallelStream()
-            .forEach(
-                channel -> {
-                  if (channel.isValid()) {
-                    ChannelData<T> data = channel.getData(filePath);
-                    if (data != null) {
-                      channelsDataPerTable.add(data);
-                      totalBufferSize.updateAndGet(v -> v + data.getBufferSize());
-                    }
-                  }
-                });
+        List<ChannelData<T>> channelsDataPerTable = new ArrayList<>();
+        // TODO: we could do parallel stream to get the channelData if needed
+        for (SnowflakeStreamingIngestChannelInternal<T> channel : table.values()) {
+          if (channel.isValid()) {
+            ChannelData<T> data = channel.getData(filePath);
+            if (data != null) {
+              channelsDataPerTable.add(data);
+              totalBufferSize += data.getBufferSize();
+            }
+          }
+        }
         if (!channelsDataPerTable.isEmpty()) {
           blobData.add(channelsDataPerTable);
         }
