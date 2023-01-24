468830cacb7e639b98508e7ce68334ea1cad2c96
Gloria Doci <gloria.doci@snowflake.com>
2023-01-24 11:49:26 +0000

    add multiple runs to exp

--- src/main/java/net/snowflake/ingest/streaming/example/ParquetPerf.java
+++ src/main/java/net/snowflake/ingest/streaming/example/ParquetPerf.java
@@ -2,52 +2,76 @@ package net.snowflake.ingest.streaming.example;
 
 import java.util.Arrays;
 import java.util.Collection;
+
 import net.snowflake.ingest.utils.Constants;
 
 public class ParquetPerf {
 
-  public static Collection<Object[]> getParameters() {
-    return Arrays.asList(
-        new Object[][] {
-          // 1M X 1
-          {"Arrow", false, Constants.BdecVersion.ONE, 10000, 100, 1},
-          {"Parquet", false, Constants.BdecVersion.THREE, 10000, 100, 1},
-          {"Parquet", true, Constants.BdecVersion.THREE, 10000, 100, 1},
-          // 1M X 2
-          {"Arrow", false, Constants.BdecVersion.ONE, 10000, 100, 2},
-          {"Parquet", false, Constants.BdecVersion.THREE, 10000, 100, 2},
-          {"Parquet", true, Constants.BdecVersion.THREE, 10000, 100, 2},
-          // 1M X 2
-          {"Arrow", false, Constants.BdecVersion.ONE, 10000, 100, 3},
-          {"Parquet", false, Constants.BdecVersion.THREE, 10000, 100, 3},
-          {"Parquet", true, Constants.BdecVersion.THREE, 10000, 100, 3},
-        });
-  }
+    private static int n_10_k = 10 * 1000;
+    private static int n_100_k = 100 * 1000;
+    private static int n_1_M = 1000 * 1000;
+    private static int numRuns = 4;
 
-  public static void main(String[] args) {
-    Collection<Object[]> param = getParameters();
+    public static Collection<Object[]> getParameters() {
+        return Arrays.asList(
+                new Object[][]{
 
-    boolean[] nullables = {true, false};
-    for (boolean n : nullables) {
-      for (Object[] p : param) {
-        SnowflakeStreamingIngestParquetPerfRunner perfRunner =
-            new SnowflakeStreamingIngestParquetPerfRunner(
-                (String) p[0],
-                (Boolean) p[1],
-                (Constants.BdecVersion) p[2],
-                (Integer) p[3],
-                (Integer) p[4],
-                (Integer) p[5],
-                n);
-        try {
-          perfRunner.setup();
-          perfRunner.runPerfExperiment();
-          perfRunner.tearDown();
-          System.out.println("Run with " + Arrays.toString(p));
-        } catch (Exception e) {
-          throw new RuntimeException(e);
-        }
-      }
+                        // 1M X 1
+                        {"Arrow", false, Constants.BdecVersion.ONE, n_10_k, 100, 1},
+                        {"Parquet", false, Constants.BdecVersion.THREE, n_10_k, 100, 1},
+                        {"Parquet", true, Constants.BdecVersion.THREE, n_10_k, 100, 1},
+                        // 1M X 2
+                        {"Arrow", false, Constants.BdecVersion.ONE, n_10_k, 100, 2},
+                        {"Parquet", false, Constants.BdecVersion.THREE, n_10_k, 100, 2},
+                        {"Parquet", true, Constants.BdecVersion.THREE, n_10_k, 100, 2},
+                        // 1M X 2
+                        {"Arrow", false, Constants.BdecVersion.ONE, n_10_k, 100, 3},
+                        {"Parquet", false, Constants.BdecVersion.THREE, n_10_k, 100, 3},
+                        {"Parquet", true, Constants.BdecVersion.THREE, n_10_k, 100, 3},
+
+                        // 100k x 10
+                        {"Arrow", false, Constants.BdecVersion.ONE, n_10_k, 10, 10},
+                        {"Parquet", false, Constants.BdecVersion.THREE, n_10_k, 10, 10},
+                        {"Parquet", true, Constants.BdecVersion.THREE, n_10_k, 10, 10},
+                        // 1M X 1
+                        {"Arrow", false, Constants.BdecVersion.ONE, n_10_k, 100, 1},
+                        {"Parquet", false, Constants.BdecVersion.THREE, n_10_k, 100, 1},
+                        {"Parquet", true, Constants.BdecVersion.THREE, n_10_k, 100, 1},
+                        // 10k x 100
+                        {"Arrow", false, Constants.BdecVersion.ONE, n_10_k, 1, 100},
+                        {"Parquet", false, Constants.BdecVersion.THREE, n_10_k, 1, 100},
+                        {"Parquet", true, Constants.BdecVersion.THREE, n_10_k, 1, 100},
+                });
     }
-  }
+
+    public static void main(String[] args) {
+        Collection<Object[]> param = getParameters();
+
+        boolean[] nullables = {true, false};
+        for (boolean n : nullables) {
+            for (Object[] p : param) {
+                for (int i = 0; i < numRuns; i++) {
+                    SnowflakeStreamingIngestParquetPerfRunner perfRunner =
+                            new SnowflakeStreamingIngestParquetPerfRunner(
+                                    (String) p[0],
+                                    (Boolean) p[1],
+                                    (Constants.BdecVersion) p[2],
+                                    (Integer) p[3],
+                                    (Integer) p[4],
+                                    (Integer) p[5],
+                                    n);
+                    try {
+
+                        perfRunner.setup();
+                        perfRunner.runPerfExperiment();
+                        perfRunner.tearDown();
+                        System.out.println("Run with " + Arrays.toString(p));
+
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
 }
