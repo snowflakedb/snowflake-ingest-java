187e9c71a922819a3637a6d9077a2cbe98d44645
Gloria Doci <gloria.doci@snowflake.com>
2023-01-30 08:37:43 +0000

    Arrow runs

--- src/main/java/net/snowflake/ingest/streaming/example/ParquetPerf.java
+++ src/main/java/net/snowflake/ingest/streaming/example/ParquetPerf.java
@@ -16,12 +16,16 @@ public class ParquetPerf {
         return Arrays.asList(
                 new Object[][]{
                         // 100k x 10
+                        {"Arrow", false, Constants.BdecVersion.ONE, n_10_k, 10, 10},
                         {"Parquet", false, Constants.BdecVersion.THREE, n_10_k, 10, 10},
-                        // 1M X 1
-                        {"Parquet", false, Constants.BdecVersion.THREE, n_10_k, 100, 1},
                         // 10k x 100
+                        {"Arrow", false, Constants.BdecVersion.ONE, n_10_k, 1, 100},
                         {"Parquet", false, Constants.BdecVersion.THREE, n_10_k, 1, 100},
+                        // 1M X 1
+                        {"Arrow", false, Constants.BdecVersion.ONE, n_10_k, 100, 1},
+                        {"Parquet", false, Constants.BdecVersion.THREE, n_10_k, 100, 1},
                         // 1M X 2
+                        {"Arrow", false, Constants.BdecVersion.ONE, n_10_k, 100, 2},
                         {"Parquet", false, Constants.BdecVersion.THREE, n_10_k, 100, 2},
                         // 1M x 3
                         {"Arrow", false, Constants.BdecVersion.ONE, n_10_k, 100, 3},
--- src/main/java/net/snowflake/ingest/streaming/example/SnowflakeStreamingIngestParquetPerfRunner.java
+++ src/main/java/net/snowflake/ingest/streaming/example/SnowflakeStreamingIngestParquetPerfRunner.java
@@ -32,7 +32,7 @@ public class SnowflakeStreamingIngestParquetPerfRunner {
   private final int numChannels;
 
   private boolean nullable;
-  private String clientName = "EC2_GDOCI_PERF_";
+  private String clientName = "GZIP_EC2_GDOCI_PERF_";
 
   public SnowflakeStreamingIngestParquetPerfRunner(
       @SuppressWarnings("unused") String name,
