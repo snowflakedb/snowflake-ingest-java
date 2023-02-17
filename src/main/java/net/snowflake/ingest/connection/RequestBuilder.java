089e5958ab78374ea19fa234f8c9cdc0b406efdf
Toby Zhang <toby.zhang@snowflake.com>
2023-02-17 13:52:50 -0800

    V1.0.3-beta.1 release (#335)
    The 1.0.3-beta release has an issue with GCS, so we have to revert #334 and then create a new release on top

--- src/main/java/net/snowflake/ingest/connection/RequestBuilder.java
+++ src/main/java/net/snowflake/ingest/connection/RequestBuilder.java
@@ -127,7 +127,7 @@ public class RequestBuilder {
   // Don't change!
   public static final String CLIENT_NAME = "SnowpipeJavaSDK";
 
-  public static final String DEFAULT_VERSION = "1.0.3-beta";
+  public static final String DEFAULT_VERSION = "1.0.3-beta.1";
 
   public static final String JAVA_USER_AGENT = "JAVA";
 
