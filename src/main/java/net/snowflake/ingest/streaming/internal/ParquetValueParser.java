98ee8d942763dde9c05315765ad0fbba3364a736
Gloria Doci <gloria.doci@snowflake.com>
2023-02-20 18:06:49 +0100

    add comment about definition level encoding length

--- src/main/java/net/snowflake/ingest/streaming/internal/ParquetValueParser.java
+++ src/main/java/net/snowflake/ingest/streaming/internal/ParquetValueParser.java
@@ -21,8 +21,29 @@ class ParquetValueParser {
   // Parquet uses BitPacking to encode boolean, hence 1 bit per value
   public static final float BIT_ENCODING_BYTE_LEN = 1.0f / 8;
 
-  /* (on average) 2 bytes / 8 values for the RLE+bitpack encoded definition level.
-  Since we don't have nested types, repetition level is always 0 and is not stored at all by Parquet. */
+  /**
+   * On average parquet needs 2 bytes / 8 values for the RLE+bitpack encoded definition level.
+   *
+   * <ul>
+   *   There are two cases how definition level (0 for null values, 1 for non-null values) is
+   *   encoded:
+   *   <li>If there are at least 8 repeated values in a row, they are run-length encoded (length +
+   *       value itself). E.g. 11111111 -> 8 1
+   *   <li>If there are less than 8 repeated values, they are written in group as part of a
+   *       bit-length encoded run, e.g. 1111 -> 15 A bit-length encoded run ends when either 64
+   *       groups of 8 values have been written or if a new RLE run starts.
+   *       <p>To distinguish between RLE and bitpack run, there is 1 extra bytes written as header
+   *       when a bitpack run starts.
+   * </ul>
+   *
+   * <ul>
+   *   For more details see ColumnWriterV1#createDLWriter and {@link
+   *   org.apache.parquet.column.values.rle.RunLengthBitPackingHybridEncoder#writeInt(int)}
+   * </ul>
+   *
+   * <p>Since we don't have nested types, repetition level is always 0 and is not stored at all by
+   * Parquet.
+   */
   public static final float DEFINITION_LEVEL_ENCODING_BYTE_LEN = 2.0f / 8;
 
   // Parquet stores length in 4 bytes before the actual data bytes
