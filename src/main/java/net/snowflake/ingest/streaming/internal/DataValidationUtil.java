08679ed20dc459e5e3e0094c657784b09a437954
Lukas Sembera <lukas.sembera@snowflake.com>
2023-02-24 09:01:03 +0100

    SNOW-749216 Allow leading and trailing whitespace
    In order ot be compatible with Snowflake, we should allow surrounding
    whitespace around string input values.

--- src/main/java/net/snowflake/ingest/streaming/internal/DataValidationUtil.java
+++ src/main/java/net/snowflake/ingest/streaming/internal/DataValidationUtil.java
@@ -343,7 +343,7 @@ class DataValidationUtil {
     }
 
     if (input instanceof String) {
-      String stringInput = (String) input;
+      String stringInput = ((String) input).trim();
       {
         // First, try to parse ZonedDateTime
         ZonedDateTime zoned = catchParsingError(() -> ZonedDateTime.parse(stringInput));
@@ -532,7 +532,8 @@ class DataValidationUtil {
       return BigDecimal.valueOf(((Number) input).doubleValue());
     } else if (input instanceof String) {
       try {
-        return new BigDecimal((String) input);
+        final String stringInput = ((String) input).trim();
+        return new BigDecimal(stringInput);
       } catch (NumberFormatException e) {
         throw valueFormatNotAllowedException(columnName, input, "NUMBER", "Not a valid number");
       }
@@ -590,7 +591,8 @@ class DataValidationUtil {
       System.arraycopy(originalInputArray, 0, output, 0, originalInputArray.length);
     } else if (input instanceof String) {
       try {
-        output = Hex.decodeHex((String) input);
+        String stringInput = ((String) input).trim();
+        output = Hex.decodeHex(stringInput);
       } catch (DecoderException e) {
         throw valueFormatNotAllowedException(columnName, input, "BINARY", "Not a valid hex string");
       }
@@ -627,7 +629,7 @@ class DataValidationUtil {
     } else if (input instanceof OffsetTime) {
       return validateAndParseTime(columnName, ((OffsetTime) input).toLocalTime(), scale);
     } else if (input instanceof String) {
-      String stringInput = (String) input;
+      String stringInput = ((String) input).trim();
       {
         // First, try to parse LocalTime
         LocalTime localTime = catchParsingError(() -> LocalTime.parse(stringInput));
@@ -712,7 +714,7 @@ class DataValidationUtil {
     } else if (input instanceof Number) {
       return ((Number) input).doubleValue();
     } else if (input instanceof String) {
-      String stringInput = (String) input;
+      String stringInput = ((String) input).trim();
       try {
         return Double.parseDouble(stringInput);
       } catch (NumberFormatException err) {
@@ -776,8 +778,8 @@ class DataValidationUtil {
       Sets.newHashSet("1", "0", "yes", "no", "y", "n", "t", "f", "true", "false", "on", "off");
 
   private static boolean convertStringToBoolean(String columnName, String value) {
-    String lowerCasedValue = value.toLowerCase();
-    if (!allowedBooleanStringsLowerCased.contains(lowerCasedValue)) {
+    String normalizedInput = value.toLowerCase().trim();
+    if (!allowedBooleanStringsLowerCased.contains(normalizedInput)) {
       throw valueFormatNotAllowedException(
           columnName,
           value,
@@ -786,12 +788,12 @@ class DataValidationUtil {
               + " https://docs.snowflake.com/en/sql-reference/data-types-logical.html#conversion-to-boolean"
               + " for the list of supported formats");
     }
-    return "1".equals(lowerCasedValue)
-        || "yes".equals(lowerCasedValue)
-        || "y".equals(lowerCasedValue)
-        || "t".equals(lowerCasedValue)
-        || "true".equals(lowerCasedValue)
-        || "on".equals(lowerCasedValue);
+    return "1".equals(normalizedInput)
+        || "yes".equals(normalizedInput)
+        || "y".equals(normalizedInput)
+        || "t".equals(normalizedInput)
+        || "true".equals(normalizedInput)
+        || "on".equals(normalizedInput);
   }
 
   /**
