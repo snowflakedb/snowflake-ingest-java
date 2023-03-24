0e8e8beba8bc982fcf6a17e93586c5d0d78c13f5
Lukas Sembera <lukas.sembera@snowflake.com>
2023-03-24 11:44:20 +0100

    reformat

--- src/main/java/net/snowflake/ingest/streaming/internal/LiteralQuoteUtils.java
+++ src/main/java/net/snowflake/ingest/streaming/internal/LiteralQuoteUtils.java
@@ -46,7 +46,9 @@ class LiteralQuoteUtils {
       return unquotedColumnNamesCache.get(columnName);
     } catch (ExecutionException e) {
       throw new SFException(
-          e, ErrorCode.INTERNAL_ERROR, String.format("Exception thrown while unquoting column name %s", columnName));
+          e,
+          ErrorCode.INTERNAL_ERROR,
+          String.format("Exception thrown while unquoting column name %s", columnName));
     }
   }
 
