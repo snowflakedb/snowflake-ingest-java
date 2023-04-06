e8da46b6948a94ae67b6bb6be6533642432a96b2
Toby Zhang <toby.zhang@snowflake.com>
2023-04-05 22:07:25 -0700

    update example

--- src/main/java/net/snowflake/ingest/streaming/example/SnowflakeStreamingIngestExample.java
+++ src/main/java/net/snowflake/ingest/streaming/example/SnowflakeStreamingIngestExample.java
@@ -6,20 +6,17 @@ package net.snowflake.ingest.streaming.example;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 import net.snowflake.ingest.streaming.InsertValidationResponse;
 import net.snowflake.ingest.streaming.OpenChannelRequest;
 import net.snowflake.ingest.streaming.SnowflakeStreamingIngestChannel;
 import net.snowflake.ingest.streaming.SnowflakeStreamingIngestClient;
 import net.snowflake.ingest.streaming.SnowflakeStreamingIngestClientFactory;
-import org.apache.hadoop.util.StopWatch;
 
 /**
  * Example on how to use the Streaming Ingest client APIs.
@@ -27,65 +24,13 @@ import org.apache.hadoop.util.StopWatch;
  * <p>Please read the README.md file for detailed steps
  */
 public class SnowflakeStreamingIngestExample {
-
+  // Please follow the example in profile_streaming.json.example to see the required properties, or
+  // if you have already set up profile.json with Snowpipe before, all you need is to add the "role"
+  // property.
   private static String PROFILE_PATH = "profile.json";
   private static final ObjectMapper mapper = new ObjectMapper();
 
-  private enum ETabType {
-    VARCHAR,
-    INT,
-    NUM38,
-    DATE
-  }
-
-  // Below are the seeting which we can control.
-
-  /** Indicates how many columns are required for the table */
-  private static final int nDataCols = 30;
-  // Col len must be at least 30
-  /** Indicates column length */
-  private static final int dataColLen = 100;
-  /** Indicates how many rows are needed */
-  private static final int numRows = 2000000;
-
-  /** Indicates the data type for each column */
-  private static final ETabType tabType = ETabType.VARCHAR;
-  /** setting to true will drop the existing table */
-  private boolean DROP_TABLES = true;
-  /** setting to true will create a new table */
-  private boolean CREATE_TABLES = true;
-  /** setting to true will truncate a existing table */
-  private boolean TRUNCATE_TABLES = false;
-
-  /** setting to true will insert data into the table via snowpipe streaming */
-  private boolean INSERT_TABLES = true;
-
-  /** setting to true will use the quotes for the column during table creation and data insert */
-  private static boolean ENABLE_QUOTES = false;
-  /**
-   * setting to true will use the ArrowBuffer. This flag is only needed when using SDK version
-   * >1.1.0
-   */
-  private static boolean USE_ARROW = false;
-
-  // Connection properties
-  private static String USER_NAME = "NOUFALBA";
-  private static String URL = "https://informatica.eu-central-1.snowflakecomputing.com:443";
-  private static String PRIVATE_KEY_FILE_LOCATION = "C:\\snowflake\\key\\rsa_streaming_key.p8";
-  private static String PORT = "443";
-  private static String SCHEME = "https";
-  private static String ROLE = "SYSADMIN";
-  private static String DATA_BASE = "testdb_kafka";
-  private static String SCHEMA = "kafka_test";
-  private static String WARE_HOUSE = "DBMI_WH1";
-  private String pad;
-  private String columnNamesArray[];
-
   public static void main(String[] args) throws Exception {
-    new SnowflakeStreamingIngestExample().doIt();
-  }
-
-  private static Properties getKeysPairAuthParams(boolean isStreamConnection) throws IOException {
     Properties props = new Properties();
     Iterator<Map.Entry<String, JsonNode>> propIt =
         mapper.readTree(new String(Files.readAllBytes(Paths.get(PROFILE_PATH)))).fields();
@@ -93,190 +38,62 @@ public class SnowflakeStreamingIngestExample {
       Map.Entry<String, JsonNode> prop = propIt.next();
       props.put(prop.getKey(), prop.getValue().asText());
     }
-    return props;
-  }
 
-  public void doIt() throws Exception {
+    // Create a streaming ingest client
+    try (SnowflakeStreamingIngestClient client =
+        SnowflakeStreamingIngestClientFactory.builder("MY_CLIENT").setProperties(props).build()) {
 
-    if (dataColLen < 30) {
-      throw new IllegalArgumentException("Col len must be >=30");
-    }
+      // Create an open channel request on table MY_TABLE, note that the corresponding
+      // db/schema/table needs to be present
+      // Example: create or replace table MY_TABLE(c1 number);
+      OpenChannelRequest request1 =
+          OpenChannelRequest.builder("MY_CHANNEL")
+              .setDBName("MY_DATABASE")
+              .setSchemaName("MY_SCHEMA")
+              .setTableName("MY_TABLE")
+              .setOnErrorOption(
+                  OpenChannelRequest.OnErrorOption.CONTINUE) // Another ON_ERROR option is ABORT
+              .build();
 
-    if (dataColLen % 10 != 0) {
-      throw new IllegalArgumentException("Col len must be a multiple of 10");
-    }
+      // Open a streaming ingest channel from the given client
+      SnowflakeStreamingIngestChannel channel1 = client.openChannel(request1);
 
-    final StringBuilder padBuilder = new StringBuilder();
-    for (int i = 0; i < dataColLen; ++i) {
-      padBuilder.append("X");
-    }
-    pad = padBuilder.toString();
+      // Insert rows into the channel (Using insertRows API)
+      final int totalRowsInTable = 1000;
+      for (int val = 0; val < totalRowsInTable; val++) {
+        Map<String, Object> row = new HashMap<>();
 
-    // get all column names and cache it
-    columnNamesArray = new String[nDataCols];
-    for (int i = 0; i < nDataCols; ++i) {
-      columnNamesArray[i] = getColName(i + 1);
-    }
+        // c1 corresponds to the column name in table
+        row.put("c1", val);
 
-    if (INSERT_TABLES) {
-      new Inserter().doInserts();
-    }
-
-    System.out.println("Done");
-  }
-
-  private String getColDef() {
-    String colDef;
-    switch (tabType) {
-      case VARCHAR:
-        colDef = String.format("varchar(%s)", dataColLen);
-        break;
-      case NUM38:
-        colDef = String.format("NUMBER(%s)", 38);
-        break;
-      case INT:
-        colDef = "INTEGER";
-        break;
-      case DATE:
-        colDef = "DATE";
-        break;
-      default:
-        throw new RuntimeException("Unsupported : " + tabType);
-    }
-
-    return colDef;
-  }
-
-  private String getFullyQualifiedTableName() {
-    return String.format("%s.%s", SCHEMA, getTabName());
-  }
-
-  private String getTabName() {
-    int tabNum = 1;
-    String tabName;
-
-    switch (tabType) {
-      case VARCHAR:
-        tabName = String.format("tabL%06d", tabNum);
-        break;
-      default:
-        throw new RuntimeException("Unsupported : " + tabType);
-    }
-    return tabName;
-  }
-
-  private String getColName(int colNum) {
-    if (ENABLE_QUOTES) {
-      return wrap(String.format("Col_%04d", colNum));
-    } else {
-      return String.format("Col_%04d", colNum);
-    }
-  }
-
-  public static String wrap(String identifier) {
-    final String quote = "\"";
-    return new StringBuilder(quote).append(identifier).append(quote).toString();
-  }
-
-  ///////////////////////////////
-  private class Inserter {
-
-    public Inserter() {}
-
-    public void doInserts() throws Exception {
-      try (SnowflakeStreamingIngestClient client =
-          SnowflakeStreamingIngestClientFactory.builder("INFA_CLIENT")
-              .setProperties(getKeysPairAuthParams(true))
-              .build()) {
-        // Open a streaming ingest channel from the given client
-        OpenChannelRequest request1 =
-            OpenChannelRequest.builder("MSSQL_TEST_RS_84")
-                .setDBName(DATA_BASE)
-                .setSchemaName(SCHEMA)
-                .setTableName("t_streamingingest")
-                .setOnErrorOption(
-                    OpenChannelRequest.OnErrorOption.CONTINUE) // Another ON_ERROR option is ABORT
-                .build();
-
-        // Open a streaming ingest channel from the given client
-        SnowflakeStreamingIngestChannel channel1 = client.openChannel(request1);
-
-        String previousOffsetTokenFromSnowflake = channel1.getLatestCommittedOffsetToken();
-
-        System.out.println(
-            "==============================================================================");
-        System.out.println(
-            "********************************  STARTING OFFSET IS "
-                + previousOffsetTokenFromSnowflake);
-        System.out.println(
-            "======================================f========================================");
-
-        // Insert rows into the channel (Using insertRows API)
-        StopWatch watch = new StopWatch();
-        watch.start();
-
-        for (int val = 0; val < numRows; val++) {
-          Map<String, Object> row = new HashMap<>();
-          for (int bc = 0; bc < nDataCols; ++bc) {
-
-            row.put(columnNamesArray[bc], buildDataCol());
-          }
-          InsertValidationResponse response = channel1.insertRow(row, String.valueOf(val + 1));
-          if (response.hasErrors()) {
-            // Simply throw if there is an exception, or you can do whatever you want with the
-            // erroneous row
-            throw response.getInsertErrors().get(0).getException();
-          }
+        // Insert the row with the current offset_token
+        InsertValidationResponse response = channel1.insertRow(row, String.valueOf(val));
+        if (response.hasErrors()) {
+          // Simply throw if there is an exception, or you can do whatever you want with the
+          // erroneous row
+          throw response.getInsertErrors().get(0).getException();
         }
-
-        System.out.println("aaaaaaaaa Elapsed Time in Seconds: " + watch.now(TimeUnit.SECONDS));
-
-        // If needed, you can check the offset_token registered in Snowflake to make sure everything
-        // is committed
-        final String expectedOffsetTokenInSnowflake = String.valueOf(numRows);
-        final int maxRetries = 60;
-        int retryCount = 0;
-
-        do {
-          String offsetTokenFromSnowflake = channel1.getLatestCommittedOffsetToken();
-          System.out.println(
-              "==============================================================================");
-          System.out.println(
-              "+++++++++++++++++++++++++++++++++++++++++  CURRENT OFFSET IS "
-                  + offsetTokenFromSnowflake);
-          System.out.println(
-              "==============================================================================");
-          if (offsetTokenFromSnowflake != null
-              && offsetTokenFromSnowflake.equals(String.valueOf(expectedOffsetTokenInSnowflake))) {
-            System.out.println(
-                "==============================================================================");
-            System.out.println(
-                "+++++++++++++++++++++++++++++++++++++++++  SUCCESSFULLY inserted "
-                    + numRows
-                    + " rows");
-            System.out.println(
-                "==============================================================================");
-            break;
-          }
-
-          retryCount++;
-        } while (true);
-        watch.stop();
-        System.out.println("aaaaaaaaa Elapsed Time in Seconds: " + watch.now(TimeUnit.SECONDS));
       }
-    }
 
-    private Object buildDataCol() {
-      Object dataVal;
-      switch (tabType) {
-        case VARCHAR:
-          dataVal = pad;
+      // If needed, you can check the offset_token registered in Snowflake to make sure everything
+      // is committed
+      final int expectedOffsetTokenInSnowflake = totalRowsInTable - 1; // 0 based offset_token
+      final int maxRetries = 10;
+      int retryCount = 0;
+
+      do {
+        String offsetTokenFromSnowflake = channel1.getLatestCommittedOffsetToken();
+        if (offsetTokenFromSnowflake != null
+            && offsetTokenFromSnowflake.equals(String.valueOf(expectedOffsetTokenInSnowflake))) {
+          System.out.println("SUCCESSFULLY inserted " + totalRowsInTable + " rows");
           break;
-        default:
-          throw new RuntimeException("Unsupported : " + tabType);
-      }
+        }
+        retryCount++;
+      } while (retryCount < maxRetries);
 
-      return dataVal;
+      // Close the channel, the function internally will make sure everything is committed (or throw
+      // an exception if there is any issue)
+      channel1.close().get();
     }
   }
 }
