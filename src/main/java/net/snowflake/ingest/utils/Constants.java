43efbf91157826c98d45d395508ea3be631f1b4e
Toby Zhang <toby.zhang@snowflake.com>
2023-04-14 12:55:36 -0700

    NO-SNOW: Preserve old JDBC behavior after upgrade (#475)
    Follow-up PR to preserve the old JDBC behavior for understore account name after #474

--- src/main/java/net/snowflake/ingest/utils/Constants.java
+++ src/main/java/net/snowflake/ingest/utils/Constants.java
@@ -24,7 +24,6 @@ public class Constants {
   public static final String ROLE = "role";
   public static final String PRIVATE_KEY = "private_key";
   public static final String PRIVATE_KEY_PASSPHRASE = "private_key_passphrase";
-  public static final String JDBC_PRIVATE_KEY = "privateKey";
   public static final String PRIMARY_FILE_ID_KEY =
       "primaryFileId"; // Don't change, should match Parquet Scanner
   public static final long RESPONSE_SUCCESS = 0L; // Don't change, should match server side
