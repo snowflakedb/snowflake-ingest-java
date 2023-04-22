a5cea37bba1396d67d563e3e9ed80d858b760201
revi cheng <revi.cheng@snowflake.com>
2023-04-21 17:28:25 -0700

    works now

--- src/main/java/net/snowflake/ingest/streaming/internal/BlobStats.java
+++ src/main/java/net/snowflake/ingest/streaming/internal/BlobStats.java
@@ -5,28 +5,19 @@
 package net.snowflake.ingest.streaming.internal;
 
 import com.codahale.metrics.Timer;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.concurrent.TimeUnit;
 
 /** Latency information for a blob */
 class BlobStats {
-  private long buildDurationMs;
-  private long uploadDurationMs;
-
   // flush and register duration cannot be calculated in the client sdk we pass the start time
   // because the end time is when the request hits the server
   private long flushStartMs;
   private long registerStartMs;
 
-  @JsonProperty("build_duration_ms")
-  long getBuildDurationMs() {
-    return this.buildDurationMs;
-  }
-
-  @JsonProperty("upload_duration_ms")
-  long getUploadDurationMs() {
-    return this.uploadDurationMs;
-  }
+  private long buildDurationMs;
+  private long uploadDurationMs;
 
   @JsonProperty("flush_start_ms")
   long getFlushStartMs() {
@@ -38,16 +29,14 @@ class BlobStats {
     return this.registerStartMs;
   }
 
-  void setBuildDurationMs(Timer.Context buildLatencyContext) {
-    if (buildLatencyContext != null) {
-      this.buildDurationMs = TimeUnit.NANOSECONDS.toMillis(buildLatencyContext.stop());
-    }
+  @JsonProperty("build_duration_ms")
+  long getBuildDurationMs() {
+    return this.buildDurationMs;
   }
 
-  void setUploadDurationMs(Timer.Context uploadLatencyContext) {
-    if (uploadLatencyContext != null) {
-      this.uploadDurationMs = TimeUnit.NANOSECONDS.toMillis(uploadLatencyContext.stop());
-    }
+  @JsonProperty("upload_duration_ms")
+  long getUploadDurationMs() {
+    return this.uploadDurationMs;
   }
 
   void setFlushStartMs(long flushStartMs) {
@@ -56,5 +45,18 @@ class BlobStats {
 
   void setRegisterStartMs(long registerStartMs) {
     this.registerStartMs = registerStartMs;
+  }
+
+  void setBuildDurationMs(Timer.Context buildLatencyContext) {
+    if (buildLatencyContext != null) {
+      this.buildDurationMs = TimeUnit.NANOSECONDS.toMillis(buildLatencyContext.stop());
+    }
+  }
+
+  @JsonIgnore
+  void setUploadDurationMs(Timer.Context uploadLatencyContext) {
+    if (uploadLatencyContext != null) {
+      this.uploadDurationMs = TimeUnit.NANOSECONDS.toMillis(uploadLatencyContext.stop());
+    }
   }
 }
