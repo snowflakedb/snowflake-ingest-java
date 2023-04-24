329334d7195bbdc29542219c289f5f50f7a7317d
Lukas Sembera <lukas.sembera@snowflake.com>
2023-04-24 12:59:19 +0000

    Add javadocs

--- src/main/java/net/snowflake/ingest/streaming/internal/MemoryInfoProvider.java
+++ src/main/java/net/snowflake/ingest/streaming/internal/MemoryInfoProvider.java
@@ -6,9 +6,12 @@ package net.snowflake.ingest.streaming.internal;
 
 /** Provider information about available system memory */
 public interface MemoryInfoProvider {
+  /** @return Max memory the JVM can allocate */
   long getMaxMemory();
 
+  /** @return Total allocated JVM memory so far */
   long getTotalMemory();
 
+  /** @return Free JVM memory */
   long getFreeMemory();
 }
