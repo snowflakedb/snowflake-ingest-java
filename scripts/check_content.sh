d72d4a95e34c32166ed0db7308ef66648ddce02f
Preston Bennes <preston.bennes@snowflake.com>
2023-05-03 23:47:22 +0000

    Fix shading class relocation: net.snowflake.ingest.shade
    - Relocate all dependency classes under net.snowflake.ingest.shade
    
    As of release 1.1.3 the shaded jar contains a large number of unrelocated
    classes. This is problematic for library consumers that include any of the same
    dependencies in their projects that snowflake-ingest-sdk uses in addition to
    snowflake-ingest-sdk. This can result in duplicate classes in a project and
    confusing errors for users depending on dependency ordering of their project.
    When duplicate classes are present on the classpath the first class found wins.
    
    This means that for consumers of snowflake-ingest-sdk if it's a dependency
    listed high in a pom.xml's dependency section, its duplicate classes take
    precedence, overriding any other dependency definitions in their project.
    
    Here's a short example of the problem. If I have a project using the ingest SDK
    with a pom.xml as follows:
    
    ```
          <dependency>
            <groupId>net.snowflake</groupId>
            <artifactId>snowflake-ingest-sdk</artifactId>
            <version>1.1.3</version>
          </dependency>
          <dependency>
            <groupId>com.google.protobuf</groupId>
            <artifactId>protobuf-java</artifactId>
            <version>3.22.3</version>
          </dependency>
    ```
    
    snowflake-ingest-sdk includes class files from
    com.google.protobuf:protobuf-java:2.5.0 which is pulled in transitively from
    hadoop-common. For any class files that are duplicates of protobuf-java 3.22.3
    the class files of 2.5.0 will take precedence at runtime due to
    snowflake-ingest-sdk being first in the pom.xml and therefore first on the
    classpath.
    
    Conversely if snowflake-ingest-sdk is last then protobuf-java 3.22.3 will be
    active which may cause runtime issues with snowflake-ingest-sdk if it depends on
    APIs only present in 2.5.0.
    
    Fixes #356

--- scripts/check_content.sh
+++ scripts/check_content.sh
@@ -1,33 +1,39 @@
-#!/bin/bash -e
+#!/bin/bash
 
-# scripts used to check if all dependency is shaded into snowflake internal path
-
+set -o errexit
+set -o nounset
 set -o pipefail
 
+# no-op comment
+: <<'EOF'
+This script checks the shade tar and verifies that it only contains classes
+under the package net.snowflake.ingest. This is to prevent class conflicts
+when the shaded jar is used in other projects.
+EOF
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-if jar tvf $DIR/../target/snowflake-ingest-sdk.jar  | awk '{print $8}' | grep -v -E "^(net|com)/snowflake" \
-    | grep -v -E "(com|net)/\$" | grep -v -E "^META-INF" | grep -v -E "^mozilla" | grep -v mime.types \
-    | grep -v project.properties | grep -v -E "javax" | grep -v -E "^org/" | grep -v -E "^com/google" \
-    | grep -v -E "^com/sun" | grep -v "log4j.properties" | grep -v "git.properties" | grep -v "io/" \
-    | grep -v "codegen/" | grep -v "com/codahale/" | grep -v "com/ibm/" | grep -v "LICENSE" | grep -v "aix/" \
-    | grep -v "darwin/" | grep -v "win/" | grep -v "freebsd/" | grep -v "linux/" | grep -v "com/github/" \
-    | grep -v -E "shaded/" | grep -v "webapps/"  | grep -v "microsoft/" | grep -v "com/ctc/" | grep -v "jersey/" \
-    | grep -v "edu/" | grep -v "com/jcraft/" | grep -v "contribs/" | grep -v "com/zaxxer/"  | grep -v "com/squareup/" \
-    | grep -v "com/thoughtworks/" | grep -v "com/jamesmurty/"  | grep -v "net/iharder/" \
-    | grep -v -E "^core-default.xml" \
-    | grep -v "yarn-version-info.properties" | grep -v "yarn-version-info.properties" \
-    | grep -v "about.html" | grep -v "jetty-dir.css" \
-    | grep -v "krb5_udp-template.conf" | grep -v "krb5-template.conf" \
-    | grep -v "ccache.txt" | grep -v "keytab.txt" \
-    | grep -v "common-version-info.properties" | grep -v "LocalizedFormats_fr.properties" \
-    | grep -v "org.apache.hadoop.application-classloader.properties" | grep -v "assets/" | grep -v "ehcache-core.xsd" \
-    | grep -v "ehcache-107ext.xsd" | grep -v "parquet.thrift" | grep -v "mapred-default.xml" \
-    | grep -v "yarn-default.xml" | grep -v ".keep"  | grep -v "NOTICE" | grep -v "digesterRules.xml" \
-    | grep -v "properties.dtd" | grep -v "PropertyList-1.0.dtd"  \
-    | grep -v "about.html" | grep -v "jetty-dir.css" | grep -v "krb5-template.conf" \
-    | grep -v "krb5_udp-template.conf" | grep -v "ccache.txt" | grep -v "keytab.txt" \
-    | grep -v "lookup.class" | grep -v "update.class" | grep -v "dig.class" | grep -v "jnamed"  \
-    ; then
-  echo "[ERROR] Ingest SDK jar includes class not under the snowflake namespace"
+
+# List all the files in the shaded snowflake-ingest-sdk.jar
+# filter for .class files
+# filter out all files under net/snowflake/ingest
+# filter out the packageless files from dnsjava
+# if any files remain, throw an error
+
+REMAINING_CLASS_FILES=$(jar tvf "${DIR}/../target/snowflake-ingest-sdk.jar" |
+  awk '{print $8}' |
+  fgrep .class |
+  egrep -v '^net/snowflake/ingest/' |
+  egrep -v '^META-INF/versions/[0-9]+/net/snowflake/ingest/' |
+  egrep -v '(dig|lookup|update|jnamed(\$[0-9])?)\.class' |
+  egrep -v 'META-INF/versions/9/module-info.class' || true)
+
+
+# error if there are any remaining class files
+# ${REMAINING_CLASS_FILES} must be quoted to print newlines instead of spaces
+# echo adds a newline so "-eq 1" means empty
+if ! [ $(echo "${REMAINING_CLASS_FILES}" | wc -l) -eq 1 ]; then
+  echo "[ERROR] Ingest SDK jar includes classes outside net.snowflake.ingest"
+  echo "Found classes in jar:"
+  echo "${REMAINING_CLASS_FILES}"
   exit 1
 fi
