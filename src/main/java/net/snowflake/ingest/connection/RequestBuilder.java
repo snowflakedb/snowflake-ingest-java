38fddbe812c4ed0e83bb632590e3b99b5da05c3a
Jay Patel <jay.patel@snowflake.com>
2023-05-17 11:54:23 -0700

    Upgrade to version 1.1.4 as well

--- src/main/java/net/snowflake/ingest/connection/RequestBuilder.java
+++ src/main/java/net/snowflake/ingest/connection/RequestBuilder.java
@@ -127,7 +127,7 @@ public class RequestBuilder {
   // Don't change!
   public static final String CLIENT_NAME = "SnowpipeJavaSDK";
 
-  public static final String DEFAULT_VERSION = "1.1.3";
+  public static final String DEFAULT_VERSION = "1.1.4";
 
   public static final String JAVA_USER_AGENT = "JAVA";
 
