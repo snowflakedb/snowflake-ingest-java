630652caef6e4c23b64fa50bdd0435d8d5910786
Andrey Zagrebin <andrey.zagrebin@snowflake.com>
2023-06-13 22:12:23 +0200

    @snow SNOW-837436 Streaming ingest: get status code message in channel status response (#525)
    Add status code message as part of the channel status response

--- src/main/java/net/snowflake/ingest/streaming/internal/SnowflakeStreamingIngestClientInternal.java
+++ src/main/java/net/snowflake/ingest/streaming/internal/SnowflakeStreamingIngestClientInternal.java
@@ -496,10 +496,12 @@ public class SnowflakeStreamingIngestClientInternal<T> implements SnowflakeStrea
                                               String.format(
                                                   "Channel has been invalidated because of failure"
                                                       + " response, name=%s, channel_sequencer=%d,"
-                                                      + " status_code=%d, executionCount=%d",
+                                                      + " status_code=%d,  message=%s,"
+                                                      + " executionCount=%d",
                                                   channelStatus.getChannelName(),
                                                   channelStatus.getChannelSequencer(),
                                                   channelStatus.getStatusCode(),
+                                                  channelStatus.getMessage(),
                                                   executionCount);
                                           logger.logWarn(errorMessage);
                                           if (getTelemetryService() != null) {
