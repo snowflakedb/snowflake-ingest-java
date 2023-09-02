6849a180c64588ea44a54fe7879d0735a1e793ac
Gloria Doci <gloria.doci@snowflake.com>
2023-09-02 10:19:27 +0200

    minor comment change

--- src/main/java/net/snowflake/ingest/streaming/internal/ClientBufferParameters.java
+++ src/main/java/net/snowflake/ingest/streaming/internal/ClientBufferParameters.java
@@ -32,10 +32,7 @@ public class ClientBufferParameters {
     this.maxAllowedRowSizeInBytes = maxAllowedRowSizeInBytes;
   }
 
-  /**
-   * @param clientInternal reference to the clientInternal object, where the relevant parameters are
-   *     set
-   */
+  /** @param clientInternal reference to the client object where the relevant parameters are set */
   public ClientBufferParameters(SnowflakeStreamingIngestClientInternal clientInternal) {
     this.enableParquetInternalBuffering =
         clientInternal != null
