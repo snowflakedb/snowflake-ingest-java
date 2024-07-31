b165b25772b2d166377c2eab2307f4bc72304096
Alec Huang <alec.huang@snowflake.com>
2024-07-31 15:47:32 -0700

    Add IcebergParquetValueParserTest for primitive columns

--- src/test/java/net/snowflake/ingest/streaming/internal/SnowflakeParquetValueParserTest.java
+++ src/test/java/net/snowflake/ingest/streaming/internal/SnowflakeParquetValueParserTest.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2024 Snowflake Computing Inc. All rights reserved.
+ */
+
 package net.snowflake.ingest.streaming.internal;
 
 import static java.time.ZoneOffset.UTC;
@@ -600,99 +604,5 @@ public class SnowflakeParquetValueParserTest {
                     0));
     Assert.assertEquals(
         "Unknown data type for logical: TIME, physical: SB16.", exception.getMessage());
-  }
-
-  /** Builder that helps to assert parsing of values to parquet types */
-  private static class ParquetValueParserAssertionBuilder {
-    private ParquetBufferValue parquetBufferValue;
-    private RowBufferStats rowBufferStats;
-    private Class valueClass;
-    private Object value;
-    private float size;
-    private Object minMaxStat;
-    private long currentNullCount;
-
-    static ParquetValueParserAssertionBuilder newBuilder() {
-      ParquetValueParserAssertionBuilder builder = new ParquetValueParserAssertionBuilder();
-      return builder;
-    }
-
-    ParquetValueParserAssertionBuilder parquetBufferValue(ParquetBufferValue parquetBufferValue) {
-      this.parquetBufferValue = parquetBufferValue;
-      return this;
-    }
-
-    ParquetValueParserAssertionBuilder rowBufferStats(RowBufferStats rowBufferStats) {
-      this.rowBufferStats = rowBufferStats;
-      return this;
-    }
-
-    ParquetValueParserAssertionBuilder expectedValueClass(Class valueClass) {
-      this.valueClass = valueClass;
-      return this;
-    }
-
-    ParquetValueParserAssertionBuilder expectedParsedValue(Object value) {
-      this.value = value;
-      return this;
-    }
-
-    ParquetValueParserAssertionBuilder expectedSize(float size) {
-      this.size = size;
-      return this;
-    }
-
-    public ParquetValueParserAssertionBuilder expectedMinMax(Object minMaxStat) {
-      this.minMaxStat = minMaxStat;
-      return this;
-    }
-
-    public ParquetValueParserAssertionBuilder expectedNullCount(long currentNullCount) {
-      this.currentNullCount = currentNullCount;
-      return this;
-    }
-
-    void assertMatches() {
-      Assert.assertEquals(valueClass, parquetBufferValue.getValue().getClass());
-      if (valueClass.equals(byte[].class)) {
-        Assert.assertArrayEquals((byte[]) value, (byte[]) parquetBufferValue.getValue());
-      } else {
-        Assert.assertEquals(value, parquetBufferValue.getValue());
-      }
-      Assert.assertEquals(size, parquetBufferValue.getSize(), 0);
-      if (minMaxStat instanceof BigInteger) {
-        Assert.assertEquals(minMaxStat, rowBufferStats.getCurrentMinIntValue());
-        Assert.assertEquals(minMaxStat, rowBufferStats.getCurrentMaxIntValue());
-        return;
-      } else if (minMaxStat instanceof byte[]) {
-        Assert.assertArrayEquals((byte[]) minMaxStat, rowBufferStats.getCurrentMinStrValue());
-        Assert.assertArrayEquals((byte[]) minMaxStat, rowBufferStats.getCurrentMaxStrValue());
-        return;
-      } else if (valueClass.equals(String.class)) {
-        // String can have null min/max stats for variant data types
-        Object min =
-            rowBufferStats.getCurrentMinStrValue() != null
-                ? new String(rowBufferStats.getCurrentMinStrValue(), StandardCharsets.UTF_8)
-                : rowBufferStats.getCurrentMinStrValue();
-        Object max =
-            rowBufferStats.getCurrentMaxStrValue() != null
-                ? new String(rowBufferStats.getCurrentMaxStrValue(), StandardCharsets.UTF_8)
-                : rowBufferStats.getCurrentMaxStrValue();
-        Assert.assertEquals(minMaxStat, min);
-        Assert.assertEquals(minMaxStat, max);
-        return;
-      } else if (minMaxStat instanceof Double || minMaxStat instanceof BigDecimal) {
-        Assert.assertEquals(minMaxStat, rowBufferStats.getCurrentMinRealValue());
-        Assert.assertEquals(minMaxStat, rowBufferStats.getCurrentMaxRealValue());
-        return;
-      }
-      throw new IllegalArgumentException(
-          String.format("Unknown data type for min stat: %s", minMaxStat.getClass()));
-    }
-
-    void assertNull() {
-      Assert.assertNull(parquetBufferValue.getValue());
-      Assert.assertEquals(currentNullCount, rowBufferStats.getCurrentNullCount());
-    }
   }
 }
