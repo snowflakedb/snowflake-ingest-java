1480525d935b4f400f055ad9a8833119b044495e
Toby Zhang <toby.zhang@snowflake.com>
2025-01-24 21:24:15 +0000

    format

--- src/main/java/net/snowflake/ingest/streaming/internal/SnowflakeStreamingIngestClientInternal.java
+++ src/main/java/net/snowflake/ingest/streaming/internal/SnowflakeStreamingIngestClientInternal.java
@@ -243,11 +243,11 @@ public class SnowflakeStreamingIngestClientInternal<T> implements SnowflakeStrea
 
     try {
       this.storageManager =
-              parameterProvider.isEnableIcebergStreaming()
-                      ? new SubscopedTokenExternalVolumeManager(
-                      this.role, this.name, this.snowflakeServiceClient)
-                      : new InternalStageManager(
-                      isTestMode, this.role, this.name, this.snowflakeServiceClient);
+          parameterProvider.isEnableIcebergStreaming()
+              ? new SubscopedTokenExternalVolumeManager(
+                  this.role, this.name, this.snowflakeServiceClient)
+              : new InternalStageManager(
+                  isTestMode, this.role, this.name, this.snowflakeServiceClient);
       this.flushService =
           new FlushService<>(this, this.channelCache, this.storageManager, this.isTestMode);
     } catch (Exception e) {
