f9dbda5b64bfeb8f4b9c558083691b07abca21e0
Angel Avalos <angel.avalos@snowflake.com>
2025-02-26 12:12:59 -0800

    testing with disabling parameter keepalive

--- deploy.sh
+++ deploy.sh
@@ -82,7 +82,7 @@ echo "[Info] Sign package and deploy to staging area"
 project_version=$($THIS_DIR/scripts/get_project_info_from_pom.py $THIS_DIR/pom.xml version)
 $THIS_DIR/scripts/update_project_version.py public_pom.xml $project_version > generated_public_pom.xml
 
-mvn deploy ${MVN_OPTIONS[@]} -Dossrh-deploy -Dmaven.wagon.http.pool=false -Dhttps.protocols=TLSv1.2 -X -e
+mvn deploy ${MVN_OPTIONS[@]} -Dossrh-deploy -Dhttp.keepAlive=false -Dhttps.protocols=TLSv1.2 -X -e
 
 # echo "[INFO] Close and Release"
 # snowflake_repositories=$(mvn ${MVN_OPTIONS[@]} \
