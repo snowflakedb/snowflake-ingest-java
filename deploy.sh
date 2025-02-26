02299330f3d04df93a672591872b15b3b9fff59a
Angel Avalos <angel.avalos@snowflake.com>
2025-02-26 11:37:13 -0800

    add parameter to use specific https protocol

--- deploy.sh
+++ deploy.sh
@@ -82,38 +82,38 @@ echo "[Info] Sign package and deploy to staging area"
 project_version=$($THIS_DIR/scripts/get_project_info_from_pom.py $THIS_DIR/pom.xml version)
 $THIS_DIR/scripts/update_project_version.py public_pom.xml $project_version > generated_public_pom.xml
 
-mvn deploy ${MVN_OPTIONS[@]} -Dossrh-deploy -Dmaven.wagon.http.pool=false -X -e
+mvn deploy ${MVN_OPTIONS[@]} -Dossrh-deploy -Dmaven.wagon.http.pool=false -Dhttps.protocols=TLSv1.2 -X -e
 
-echo "[INFO] Close and Release"
-snowflake_repositories=$(mvn ${MVN_OPTIONS[@]} \
-    org.sonatype.plugins:nexus-staging-maven-plugin:1.6.7:rc-list \
-    -DserverId=$MVN_REPOSITORY_ID \
-    -DnexusUrl=https://oss.sonatype.org/ | grep netsnowflake | awk '{print $2}')
-IFS=" "
-if (( $(echo $snowflake_repositories | wc -l)!=1 )); then
-    echo "[ERROR] Not single netsnowflake repository is staged. Login https://oss.sonatype.org/ and make sure no netsnowflake remains there."
-    exit 1
-fi
-if ! mvn ${MVN_OPTIONS[@]} \
-    org.sonatype.plugins:nexus-staging-maven-plugin:1.6.7:rc-close \
-    -DserverId=$MVN_REPOSITORY_ID \
-    -DnexusUrl=https://oss.sonatype.org/ \
-    -DstagingRepositoryId=$snowflake_repositories \
-    -DstagingDescription="Automated Close"; then
-    echo "[ERROR] Failed to close. Fix the errors and try this script again"
-    mvn ${MVN_OPTIONS[@]} \
-        nexus-staging:rc-drop \
-        -DserverId=$MVN_REPOSITORY_ID \
-        -DnexusUrl=https://oss.sonatype.org/ \
-        -DstagingRepositoryId=$snowflake_repositories \
-        -DstagingDescription="Failed to close. Dropping..."
-fi
+# echo "[INFO] Close and Release"
+# snowflake_repositories=$(mvn ${MVN_OPTIONS[@]} \
+#     org.sonatype.plugins:nexus-staging-maven-plugin:1.6.7:rc-list \
+#     -DserverId=$MVN_REPOSITORY_ID \
+#     -DnexusUrl=https://oss.sonatype.org/ | grep netsnowflake | awk '{print $2}')
+# IFS=" "
+# if (( $(echo $snowflake_repositories | wc -l)!=1 )); then
+#     echo "[ERROR] Not single netsnowflake repository is staged. Login https://oss.sonatype.org/ and make sure no netsnowflake remains there."
+#     exit 1
+# fi
+# if ! mvn ${MVN_OPTIONS[@]} \
+#     org.sonatype.plugins:nexus-staging-maven-plugin:1.6.7:rc-close \
+#     -DserverId=$MVN_REPOSITORY_ID \
+#     -DnexusUrl=https://oss.sonatype.org/ \
+#     -DstagingRepositoryId=$snowflake_repositories \
+#     -DstagingDescription="Automated Close"; then
+#     echo "[ERROR] Failed to close. Fix the errors and try this script again"
+#     mvn ${MVN_OPTIONS[@]} \
+#         nexus-staging:rc-drop \
+#         -DserverId=$MVN_REPOSITORY_ID \
+#         -DnexusUrl=https://oss.sonatype.org/ \
+#         -DstagingRepositoryId=$snowflake_repositories \
+#         -DstagingDescription="Failed to close. Dropping..."
+# fi
 
-mvn ${MVN_OPTIONS[@]} \
-    org.sonatype.plugins:nexus-staging-maven-plugin:1.6.7:rc-release \
-    -DserverId=$MVN_REPOSITORY_ID \
-    -DnexusUrl=https://oss.sonatype.org/ \
-    -DstagingRepositoryId=$snowflake_repositories \
-    -DstagingDescription="Automated Release"
+# mvn ${MVN_OPTIONS[@]} \
+#     org.sonatype.plugins:nexus-staging-maven-plugin:1.6.7:rc-release \
+#     -DserverId=$MVN_REPOSITORY_ID \
+#     -DnexusUrl=https://oss.sonatype.org/ \
+#     -DstagingRepositoryId=$snowflake_repositories \
+#     -DstagingDescription="Automated Release"
 
 rm $OSSRH_DEPLOY_SETTINGS_XML
