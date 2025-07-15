7f6cd8a19a261fdfbe3de214c9d77ebf2a1d069e
Toby Zhang <toby.zhang@snowflake.com>
2025-07-15 06:49:02 +0000

    switch to use central pushlisher portal

--- unshaded_snapshot_deploy.sh
+++ unshaded_snapshot_deploy.sh
@@ -6,6 +6,11 @@ export GPG_KEY_ID="Snowflake Computing"
 export LDAP_USER="$INTERNAL_NEXUS_USERNAME"
 export LDAP_PWD="$INTERNAL_NEXUS_PASSWORD"
 
+if [ -z "$GPG_KEY_ID" ]; then
+  echo "[ERROR] Key Id not specified!"
+  exit 1
+fi
+
 if [ -z "$GPG_KEY_PASSPHRASE" ]; then
   echo "[ERROR] GPG passphrase is not specified for $GPG_KEY_ID!"
   exit 1
@@ -21,11 +26,11 @@ if ! gpg --list-secret-key | grep "$GPG_KEY_ID"; then
   gpg --allow-secret-key-import --import "$GPG_PRIVATE_KEY"
 fi
 
-# copy the settings.xml template and inject credential information
-UNSHADED_SNAPSHOT_DEPLOY_SETTINGS_XML="$THIS_DIR/mvn_settings_unshaded_snapshot_deploy.xml"
+# copy the settings.xml template and inject credential information 
+CENTRAL_DEPLOY_SETTINGS_XML="$THIS_DIR/mvn_settings_central_deploy.xml"
 MVN_REPOSITORY_ID=snapshot
 
-cat > $UNSHADED_SNAPSHOT_DEPLOY_SETTINGS_XML << SETTINGS.XML
+cat > $CENTRAL_DEPLOY_SETTINGS_XML << SETTINGS.XML
 <?xml version="1.0" encoding="UTF-8"?>
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -33,23 +38,22 @@ cat > $UNSHADED_SNAPSHOT_DEPLOY_SETTINGS_XML << SETTINGS.XML
   <servers>
     <server>
       <id>$MVN_REPOSITORY_ID</id>
-      <username>$LDAP_USER</username>
-      <password>$LDAP_PWD</password>
+      <username>$NEXUS_USER</username>
+      <password>$NEXUS_PWD</password>
     </server>
   </servers>
 </settings>
 SETTINGS.XML
 
 MVN_OPTIONS+=(
-  "--settings" "$UNSHADED_SNAPSHOT_DEPLOY_SETTINGS_XML"
+  "--settings" "$CENTRAL_DEPLOY_SETTINGS_XML"
   "--batch-mode"
 )
 
-echo "[Info] Sign unshaded package and deploy to staging area"
+echo "[INFO] Deploy to snapshot repository"
 project_version=$($THIS_DIR/scripts/get_project_info_from_pom.py $THIS_DIR/pom.xml version)
-echo "[Info] Project version: $project_version"
-$THIS_DIR/scripts/update_project_version.py pom.xml ${project_version} > generated_public_pom.xml
+$THIS_DIR/scripts/update_project_version.py public_pom.xml $project_version-unshaded > generated_public_pom.xml
 
-mvn deploy ${MVN_OPTIONS[@]} -Dnot-shadeDep -Dsnapshot-deploy
+mvn deploy ${MVN_OPTIONS[@]} -Dsnapshot-deploy -Dhttp.keepAlive=false -Dnot-shadeDep
 
-rm $UNSHADED_SNAPSHOT_DEPLOY_SETTINGS_XML
+rm $CENTRAL_DEPLOY_SETTINGS_XML
